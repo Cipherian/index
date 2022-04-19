3b11ee24c5a898a53ae26b5fc96d494032588f8a
Daniel Brott <danbrott42@gmail.com>
2022-04-18 23:38:26 -0700

    Updating this for lab 7

---
+++ app.js
@@ -0,0 +1,18 @@
+function cashmoney() {
+    confirm("YOU WIN CASH MONEY! SEND US YOUR BANK NUMBER NAME AND ADDRESS!!!");
+}
+function asktime() {
+
+    let time = prompt("What is the time?",)
+
+    if (time >= 1 && time <= 18);
+    document.write("Good day.",)
+    if (time >= 19 && time <= 24)
+        document.write("Good evening.",)
+    else return prompt("Error, please enter a valid time 0 - 24")
+     
+}
+function toCelsius(fahrenheit) {
+    return (5 / 9) * (fahrenheit - 32);
+    document.write ("degrees" + celsius)
+}
