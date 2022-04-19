25ccc46db7203e348b39d2d48926fd30d69475c7
Daniel Brott <danbrott42@gmail.com>
2022-04-19 11:06:10 -0700

    Updating this app.js for a better functioning function

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 function cashmoney() {
     confirm("YOU WIN CASH MONEY! SEND US YOUR BANK NUMBER NAME AND ADDRESS!!!");
 }
-function asktime() {
 
+
+function asktime() {
     let time = prompt("What is the time?",)
 
-    if (time >= 1 && time <= 18);
-    document.write("Good day.",)
-    if (time >= 19 && time <= 24)
-        document.write("Good evening.",)
-    else return prompt("Error, please enter a valid time 0 - 24")
-     
-}
-function toCelsius(fahrenheit) {
-    return (5 / 9) * (fahrenheit - 32);
-    document.write ("degrees" + celsius)
+    if (time > 1 && time < 18) {
+        document.write("Good day.",);
+        return;
+    }
+    else if (time > 19 && time < 24) {
+        document.write("Good evening.",);
+        return;
+    }
+    else
+        prompt("That is an invalid number, please enter a number between 0 and 24");
+    asktime()
 }
