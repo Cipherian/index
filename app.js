8229a9bb28ac84ab3a194502e242e6f0ac471773
Daniel Brott <danbrott42@gmail.com>
2022-04-19 19:01:33 -0700

    fixed errors in the operations

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ function cashmoney() {
 function asktime() {
     let time = prompt("What is the time?",)
 
-    if (time > 1 && time < 18) {
+    if (time >= 1 && time <= 18) {
         document.write("Good day.",);
         return;
     }
-    else if (time > 19 && time < 24) {
+    else if (time >= 19 && time <= 24) {
         document.write("Good evening.",);
         return;
     }
