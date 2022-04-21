63ca6ea2c74f62046f0f1a253b793d2522a31ee7
Daniel Brott <danbrott42@gmail.com>
2022-04-20 17:21:44 -0700

    updating for my 8th lab and to save the planet

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+
 function cashmoney() {
     confirm("YOU WIN CASH MONEY! SEND US YOUR BANK NUMBER NAME AND ADDRESS!!!");
 }
 
-
 function asktime() {
-    let time = prompt("What is the time?",)
+    let time = prompt("What is the time?")
 
     if (time >= 1 && time <= 18) {
-        document.write("Good day.",);
+        document.write("Good day.");
         return;
     }
     else if (time >= 19 && time <= 24) {
-        document.write("Good evening.",);
+        document.write("Good evening.");
         return;
     }
     else
         alert("That is an invalid number, please enter a number between 0 and 24");
-    asktime()
+    asktime();
+}
+
+function stars() {
+    let star = prompt("Rate this page out of 5 stars");
+
+    for (let i = 1; i <= star; i++)
+    document.write("<p>&#9734</p>");
+    if (star <=2)
+    alert('That is that is not very nice')
+    if (star >= 5)
+    alert ('That is very nice of you')
 }
